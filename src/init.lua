996cebc19b51b035121129e0c11d0e1aa6527433
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-04-22 01:31:43 -0400

    first commit

---
+++ src/init.lua
@@ -0,0 +1,40 @@
+local Debris = game:GetService("Debris")
+local TweenService = game:GetService("TweenService")
+local CollectionService = game:GetService("CollectionService")
+
+function burn(part)
+	if CollectionService:HasTag(part, "Burning") then return end
+	if part.Anchored then return end
+	CollectionService:AddTag(part, "Burning")
+	local burnSpeed = (part.Size.X * part.Size.Y * part.Size.Z)*0.5
+	-- local sound = import("sound/Weather/Fire"):Clone()
+	-- sound.Parent = part
+	-- sound.Looped = true
+	-- sound:Play()
+	local tweenInfo = TweenInfo.new(burnSpeed, Enum.EasingStyle.Quint, Enum.EasingDirection.In)
+	local fire = Instance.new("Fire", part)
+	fire.Size = 0
+	fire.Heat = 0
+	local partTween = TweenService:Create(part, tweenInfo, {
+		Transparency = 1
+	})
+	local fireTween = TweenService:Create(fire, tweenInfo, {
+		Size = math.max(part.Size.X, part.Size.Y, part.Size.Z),
+		Heat = math.max(part.Size.X, part.Size.Y, part.Size.Z)/2,
+	})
+
+	partTween:Play()
+	fireTween:Play()
+	for i, neighbor in ipairs(part:GetConnectedParts()) do
+		task.delay(burnSpeed*0.75, function()
+			burn(neighbor)
+		end)
+	end
+	-- Debris:AddItem(sound, burnSpeed)
+	Debris:AddItem(fireTween, burnSpeed)
+	Debris:AddItem(partTween, burnSpeed)
+	Debris:AddItem(part, burnSpeed)
+	Debris:AddItem(fire, burnSpeed)
+end
+
+return burn
